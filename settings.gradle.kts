3e402e45bf378f52e316d95069977ab784efcb0f
Akash Das <akashdas.dev@gmail.com>
2023-12-01 00:29:46 +0600

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Task Manager App with Alarm Reminder"
+include(":app")
+ 
